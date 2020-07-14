12d6429ba67a85241030a32e5b7690f486d355aa
Eduardo Cavalcante <epscavalcant@gmail.com>
2020-07-14 16:21:23 -0400

    .

---
+++ gulpfile.js
@@ -0,0 +1,146 @@
+"use strict";
+
+// Load plugins
+const autoprefixer = require("gulp-autoprefixer");
+const browsersync = require("browser-sync").create();
+const cleanCSS = require("gulp-clean-css");
+const del = require("del");
+const gulp = require("gulp");
+const header = require("gulp-header");
+const merge = require("merge-stream");
+const plumber = require("gulp-plumber");
+const rename = require("gulp-rename");
+const sass = require("gulp-sass");
+const uglify = require("gulp-uglify");
+const minify = require("gulp-htmlmin");
+
+// Load package.json for banner
+const pkg = require("./package.json");
+
+// Set the banner content
+const banner = [
+  "/*!\n",
+  " * Start Bootstrap - <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n",
+  " * Copyright 2013-" + new Date().getFullYear(),
+  " <%= pkg.author %>\n",
+  " * Licensed under <%= pkg.license %> (https://github.com/BlackrockDigital/<%= pkg.name %>/blob/master/LICENSE)\n",
+  " */\n",
+  "\n",
+].join("");
+
+// BrowserSync
+function browserSync(done) {
+  browsersync.init({
+    server: {
+      baseDir: "./",
+    },
+    // port: 80,
+  });
+  done();
+}
+
+// BrowserSync reload
+function browserSyncReload(done) {
+  browsersync.reload();
+  done();
+}
+
+// Clean vendor
+function clean() {
+  return del(["./vendor/"]);
+}
+
+// Bring third party dependencies from node_modules into vendor directory
+function modules() {
+  var bootstrapJS = gulp
+    .src("./node_modules/bootstrap/dist/js/bootstrap.bundle.min.js")
+    .pipe(gulp.dest("./js/"));
+
+  var owlCarousel2JS = gulp
+    .src([
+      "./node_modules/owl.carousel2/dist/owl.carousel.min.js",
+    ])
+    .pipe(gulp.dest("./js/"));
+  
+  // var fontAwesomeFonts
+
+  var jquery = gulp
+    .src([
+      "./node_modules/jquery/dist/jquery.slim.min.js",
+    ])
+    .pipe(gulp.dest("./js/"));
+
+  gulp.task('minify', () => {
+    return gulp.src('*.html')
+      .pipe(htmlmin({ collapseWhitespace: true }))
+      .pipe(gulp.dest('dist'));
+  });
+
+  return merge(
+    bootstrapJS,
+    owlCarousel2JS,
+    jquery
+  );
+}
+
+// CSS task
+function css() {
+  return gulp
+    .src("./scss/**/*.scss")
+    .pipe(plumber())
+    .pipe(
+      sass({
+        outputStyle: "expanded",
+        includePaths: "./node_modules",
+      })
+    )
+    .on("error", sass.logError)
+    .pipe(
+      autoprefixer({
+        cascade: false,
+      })
+    )
+    .pipe(cleanCSS())
+    .pipe(gulp.dest("./css"))
+    .pipe(browsersync.stream());
+}
+
+// JS task
+function js() {
+  return gulp
+    .src(["./js/*.js", "!./js/*.min.js"])
+    .pipe(uglify())
+    .pipe(
+      header(banner, {
+        pkg: pkg,
+      })
+    )
+    .pipe(
+      rename({
+        suffix: ".min",
+      })
+    )
+    .pipe(gulp.dest("./js"))
+    .pipe(browsersync.stream());
+}
+
+// Watch files
+function watchFiles() {
+  gulp.watch("./scss/**/*", css);
+  gulp.watch(["./vendor/**/*", "!./js/**/*.min.js"], js);
+  gulp.watch("./**/*.html", browserSyncReload);
+}
+
+// Define complex tasks
+const vendor = gulp.series(clean, modules);
+const build = gulp.series(vendor, gulp.parallel(css, js));
+const watch = gulp.series(build, gulp.parallel(watchFiles, browserSync));
+
+// Export tasks
+exports.css = css;
+exports.js = js;
+exports.clean = clean;
+exports.vendor = vendor;
+exports.build = build;
+exports.watch = watch;
+exports.default = build;
